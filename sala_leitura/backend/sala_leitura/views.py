cc621f317fc489c1c1d0edf2fc1d1577e0b1fe21
snowfox <souza.enrique7@gmail.com>
2025-04-10 15:51:13 -0300

    Refatorando templates, urls e views para consulta

--- sala_leitura/backend/sala_leitura/views.py
+++ sala_leitura/backend/sala_leitura/views.py
@@ -65,8 +65,6 @@ def consulta(request):
     emprestimos = Emprestimo.objects.filter(usuario=request.user)
     historicos = Emprestimo.objects.filter(usuario=request.user, data_devolucao__isnull=False)
     # Filtros
-    filtro = request.GET.get('filtro')
-    valor = request.GET.get('valor')
     busca_nome = request.GET.get('busca_nome', '')
     # Condições
     if busca_nome:
@@ -95,105 +93,6 @@ def consulta(request):
     }
     
     return render(request, 'consulta.html', context)
-
-# View para a listar livros
-@login_required
-def lista_livros(request):
-    filtro = request.GET.get('filtro')
-    valor = request.GET.get('valor')
-    livros = Livro.objects.filter(usuario=request.user)
-
-    if filtro and valor:
-        if filtro == 'registro':
-            livros = livros.filter(registro__icontains=valor)
-        elif filtro == 'autor':
-            livros = livros.filter(autor__icontains=valor)
-        elif filtro == 'titulo':
-            livros = livros.filter(titulo__icontains=valor)
-        elif filtro == 'editora':
-            livros = livros.filter(editora__icontains=valor)
-    
-    return render(request, 'lista_livros.html', {'livros': livros})
-
-# View para os detalhes do livro
-def detalhes_livro(request, id):
-    livro = get_object_or_404(Livro, id=id)
-    return render(request, 'detalhes_livro.html', {'livro': livro})
-
-# View para a listar alunos
-@login_required
-def lista_alunos(request):
-    filtro = request.GET.get('filtro')
-    valor = request.GET.get('valor')
-    alunos = Aluno.objects.filter(usuario=request.user)
-    is_sexo_filter = False  # Flag para identificar se o filtro é 'sexo'
-
-    if filtro and valor:
-        if filtro == 'nome':
-            alunos = alunos.filter(nome__icontains=valor)
-        elif filtro == 'ra':
-            alunos = alunos.filter(ra__icontains=valor)
-        elif filtro == 'sexo':
-            alunos = alunos.filter(sexo__iexact=valor)
-            is_sexo_filter = True  # Ativa a flag para o filtro de sexo
-
-    return render(request, 'lista_alunos.html', {'alunos': alunos, 'is_sexo_filter': is_sexo_filter})
-
-# View para a listar editoras
-@login_required
-def lista_editoras(request):
-    busca_nome = request.GET.get('busca_nome')
-    editoras = Editora.objects.filter(usuario=request.user)
-
-    if busca_nome:
-        editoras = editoras.filter(nome__icontains=busca_nome)
-    
-    return render(request, 'lista_editoras.html', {'editoras': editoras})
-
-# View para a listar categorias
-@login_required
-def lista_categorias(request):
-    busca_nome = request.GET.get('busca_nome')
-    categorias = Categoria.objects.filter(usuario=request.user)
-
-    if busca_nome:
-        categorias = categorias.filter(tipo__icontains=busca_nome)
-    
-    return render(request, 'lista_categorias.html', {'categorias': categorias})
-
-# View para a listar emprestimos
-@login_required
-def lista_emprestimos(request):
-    filtro = request.GET.get('filtro')
-    valor = request.GET.get('valor')
-    
-    # Inicia a consulta de empréstimos com devolução confirmada
-    historicos = Emprestimo.objects.filter(usuario=request.user, data_devolucao__isnull=False)
-
-    if filtro and valor:
-        if filtro == 'aluno':
-            historicos = historicos.filter(aluno__nome__icontains=valor)
-        elif filtro == 'titulo':
-            historicos = historicos.filter(livro__titulo__icontains=valor)
-        elif filtro == 'data_devolucao':
-            try:
-                # Tenta filtrar por data de devolução se o valor for uma data válida
-                data_devolucao = timezone.datetime.strptime(valor, "%d/%m/%Y").date()
-                historicos = historicos.filter(data_devolucao=data_devolucao)
-            except ValueError:
-                pass  # Caso não seja uma data válida, não aplica filtro
-    
-    # Carrega todas as opções de alunos e livros para o filtro
-    alunos = Aluno.objects.filter(usuario=request.user)
-    livros = Livro.objects.filter(usuario=request.user)
-
-    return render(request, 'lista_emprestimos.html', {
-        'historicos': historicos,
-        'alunos': alunos,
-        'livros': livros,
-        'filtro': filtro,
-        'valor': valor
-    })
 
 # View para cadastros dos itens
 @login_required
@@ -282,3 +181,7 @@ def devolver_livro(request, emprestimo_id):
 
     return redirect('emprestimo')
 
+# View para os detalhes do livro
+def detalhes_livro(request, id):
+    livro = get_object_or_404(Livro, id=id)
+    return render(request, 'detalhes_livro.html', {'livro': livro})
