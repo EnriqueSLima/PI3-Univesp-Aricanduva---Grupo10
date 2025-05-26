ba1dcfa4fcf03242517f08b60157c569b5f19e73
snowfox <souza.enrique7@gmail.com>
2025-05-26 20:10:24 -0300

    Muda sslmode incompatível

--- sala_leitura/backend/core/settings.py
+++ sala_leitura/backend/core/settings.py
@@ -70,8 +70,6 @@ ALLOWED_HOSTS = [host.strip() for host in allowed_hosts.split(',') if host.strip
 DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL', f'sqlite:///{BASE_DIR}/db.sqlite3'),
-        conn_max_age=600,
-        ssl_require=not DEBUG
     )
 }
 
